7109fd58ca6b3e51f41f0833b200899d8912f9c0
dip4k <dip4k.chauhan@gmail.com>
2017-10-31 07:31:18 +0530

    Added Sass and uglify Code

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,31 @@ module.exports = function(grunt) {
         dest: "build/scripts.js"
       },
       css: {
-        src: ["css/*.css"],
+        src: ["css/bootstrap.css", "css/reset.css", "css/styles.css"],
         dest: "build/styles.css"
+      }
+    },
+
+    //sass
+    sass: {
+      build: {
+        files: [
+          {
+            src: "css/sass/styles.scss",
+            dest: "css/styles.css"
+          }
+        ]
+      }
+    },
+    //Uglify
+    uglify: {
+      build: {
+        files: [
+          {
+            src: "build/scripts.js",
+            dest: "build/scripts-min.js"
+          }
+        ]
       }
     }
   });
@@ -19,7 +42,8 @@ module.exports = function(grunt) {
   //load Plugins
   //   grunt.loadNpmTasks('');
   grunt.loadNpmTasks("grunt-contrib-concat");
-
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
   //Register Task
 
   //   grunt.registerTask("all", ["run", "sleep"]);
